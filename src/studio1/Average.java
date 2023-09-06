eeecb7ab1254693606aebe1a321b986024c88102
jeongwonlee <jeongwonlee@localhost>
2023-09-06 15:34:35 -0500

    Finished Studio 1.

---
+++ src/studio1/Average.java
@@ -0,0 +1,19 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("What is the first integer to be averaged?");
+		int n1 = in.nextInt();
+		System.out.print("What is the second integer to be averaged?");
+		int n2 = in.nextInt();
+		System.out.print("Average of " + n1 + " and "+ n2 + " is " + ((n1+n2)/2.0) + ".");
+		
+	}
+
+}
